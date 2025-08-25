9285c6416d9402e81b9412572c4e5aa3325dbdce
Bani Faza Rasyadan <bnfzrsydn2@gmail.com>
2025-08-25 21:50:53 +0700

    Create a navbar

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,4 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/items.tsx")] satisfies RouteConfig;
+export default [index("routes/items.tsx"), route("new", "routes/newItems.tsx")] satisfies RouteConfig;
 
---
+++ app/routes/newItems.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const newItems = () => {
+  return (
+    <div>newItems</div>
+  )
+}
+
+export default newItems
